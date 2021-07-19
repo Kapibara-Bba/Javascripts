1c0f6b58b386fcacc99fecb27ed2c30d4cb7ef10
Masaya Furukawa <kapibara.bba0924@gmail.com>
2021-07-19 23:43:53 +0900

    [Add]Node.js

--- script.js
+++ script.js
@@ -12,3 +12,12 @@ $(function() {
   $('p').slideUp();
   
 });
+
+$(function() {
+  // slideUpメソッドを用いて、「#title」の要素を隠してください
+  $('#title').slideUp();
+  
+  // fadeOutメソッドを用いて、「.lesson-item」の要素を隠してください
+  $('.lesson-item').fadeOut();
+  
+});
