fc29a510ae3427cb42af48d8f8b3b7290b06fbfa
Masaya Furukawa <kapibara.bba0924@gmail.com>
2021-07-20 23:36:36 +0900

    [Add]Node.js

--- script.js
+++ script.js
@@ -21,3 +21,12 @@ $(function() {
   $('.lesson-item').fadeOut();
   
 });
+
+$(function() {
+  // showメソッドを用いて、「#title」要素を表示してください
+  $('#title').show();
+  
+  // fadeInメソッドを用いて、「#image」要素を表示してください
+  $('#image').fadeIn();
+  
+});
