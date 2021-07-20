e2d249ea8b46da9f639400fb1d981e123b4c66de
Masaya Furukawa <kapibara.bba0924@gmail.com>
2021-07-21 00:22:45 +0900

    [Add]Node.js

--- script.js
+++ script.js
@@ -30,3 +30,11 @@ $(function() {
   $('#image').fadeIn();
   
 });
+
+$(function() {
+  // 「#hide-text」要素に対するclickイベントを作成してください
+  $('#hide-text').click(function() {
+    $('#text').slideUp();
+  });
+  
+});
