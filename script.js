bc35659a2853344d29783e074d583b1edf269471
Masaya Furukawa <kapibara.bba0924@gmail.com>
2021-07-22 10:56:42 +0900

    [Add]Node.js

--- script.js
+++ script.js
@@ -38,3 +38,12 @@ $(function() {
   });
   
 });
+
+$(function() {
+  // 「#change-css」要素に対するclickイベントを作成してください
+  $('#change-css').click(function() {
+    $('#text').css('color','red');
+    $('#text').css('font-size','50px');
+  });
+    
+});
