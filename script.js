3e096f835a631d5093f74dd588ef99c08146c708
Masaya Furukawa <kapibara.bba0924@gmail.com>
2021-07-26 23:33:19 +0900

    [Add]Node.js

--- script.js
+++ script.js
@@ -83,3 +83,21 @@ $(function() {
     $('#text').css('color', 'blue').html('<h3>jQueryをマスターしましょう！</h3>').fadeOut(1000);
   });
 });
+
+$(function() {
+  $('#find-method').click(function() {
+    // findメソッドで、「#wrapper」内にあるすべての「a」要素を取得し、
+    // cssメソッドで文字の色をredに指定してください
+    $('#wrapper').find('a').css('color','red');
+    
+    
+  });
+  
+  $('#children-method').click(function() {
+    // childrenメソッドで、「#wrapper」の一階層下にある「a」要素を取得し、
+    // fadeOutメソッドで隠してください
+    $('#wrapper').children('a').fadeOut();
+    
+    
+  });
+});
